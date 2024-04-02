aed7e0c517b8edd9fb2ca6425740bf13992a5a20
Rubén <ruben.sr.apple@gmail.com>
2024-04-02 10:44:52 +0200

    Creada app en Xcode

---
+++ GameStream/GameStream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by Ruben on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("GameStream App is coming! 😄")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameStream/GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Ruben on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
