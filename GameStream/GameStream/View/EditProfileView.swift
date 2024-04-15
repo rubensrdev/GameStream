f1dcca83784018c65e3772a4c2bc93d265a8f22f
Rubén <ruben.sr.apple@gmail.com>
2024-04-15 11:39:27 +0200

    add edit data module

--- GameStream/GameStream/View/EditProfileView.swift
+++ GameStream/GameStream/View/EditProfileView.swift
@@ -15,6 +15,12 @@ struct EditProfileView: View {
             VStack {
                 ScrollView {
                     VStack {
+                        
+                        Text("Edición de perfil")
+                            .foregroundStyle(.darkCian)
+                            .font(.title)
+                            .bold()
+                        
                         ZStack {
                             Button(action: {}, label: {
                                 Image("perfil")
@@ -38,11 +44,102 @@ struct EditProfileView: View {
 }
 
 struct EditModule: View {
+    
+    @State var correo: String = ""
+    @State var contraseña: String = ""
+    @State var nombre: String = ""
+    @State var mostrarContraseña: Bool = false
+    
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text("Correo electrónico")
+                .foregroundStyle(.darkCian)
+            ZStack(alignment: .leading) {
+                if correo.isEmpty {
+                    Text(verbatim: "ejemplo@gmail.com")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                }
+                TextField("", text: $correo)
+                    .keyboardType(.emailAddress)
+            }
+            Divider()
+                .frame(height: 1)
+                .background(.darkCian)
+                .padding(.bottom, 12)
+            Text("Contraseña")
+                .foregroundStyle(.darkCian)
+            ZStack(alignment: .leading) {
+                HStack {
+                    if contraseña.isEmpty {
+                        Text("Escribe tu contraseña")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }
+                    if mostrarContraseña {
+                        TextField("", text: $contraseña)
+                            .foregroundStyle(.white)
+                    } else {
+                        SecureField("", text: $contraseña)
+                            .foregroundStyle(.white)
+                    }
+                    Button(action: cambiarEstadoMostrarContraseña, label: {
+                        Image(systemName: "eye.fill")
+                            .foregroundStyle(.darkCian)
+                    })
+                }
+            }
+            Divider()
+                .frame(height: 1)
+                .background(.darkCian)
+                .padding(.bottom, 12)
+            Text("Nombre")
+                .foregroundStyle(.darkCian)
+            ZStack(alignment: .leading) {
+                if nombre.isEmpty {
+                    Text("Escribe tu nombre")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                }
+                TextField("", text: $nombre)
+            }
+            Divider()
+                .frame(height: 1)
+                .background(.darkCian)
+                .padding(.bottom, 32)
+            
+            Button(action: updateData, label: {
+                Text("Actualizar datos")
+                    .textCase(.uppercase)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .padding(EdgeInsets(
+                        top: 11, leading: 18, bottom: 11, trailing: 18
+                    ))
+                    .overlay(RoundedRectangle(cornerRadius: 6).stroke(.darkCian, lineWidth: 2)
+                        .shadow(color: .white, radius: 3)
+                    )
+            })
+            //.navigationDestination(isPresented: $isPantallaHomeActive, destination: {Home()})
             
         }
+        .padding(.horizontal, 42)
     }
+    
+    func updateData() {
+        print("Guardado y recuperando datos de usuario")
+    }
+    
+    func cambiarEstadoMostrarContraseña() {
+        if mostrarContraseña {
+            mostrarContraseña = false
+        } else {
+            mostrarContraseña = true
+        }
+    }
+    
 }
 
 #Preview {
